4753820b3db71a6439f75cc8fe94ddb92192f683
John Wesley <wesleyjohn691@gmail.com>
2025-05-27 12:39:40 +0300

    Initial Commit

---
+++ code.js
@@ -0,0 +1,19 @@
+let message = "Hello, World!";
+console.log(message);
+
+let newMessage = "This is a new message!";
+console.log(newMessage);
+
+let randomNumber = 22;
+console.log("Random number is: " + randomNumber);
+
+const constantExample = "Shouln't change";
+console.log(constantExample);
+
+let objectExample = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(objectExample['age']);
